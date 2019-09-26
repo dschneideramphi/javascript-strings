bcf981ff0fde1fff4a83dd9db3000c9729e16877
dschneideramphi <dschneider@amphi.com>
2019-09-26 11:45:45 -0700

    Setup Assignment!

---
+++ cool-name.js
@@ -0,0 +1,32 @@
+//Program: Cool-Name Program
+
+//General Description:
+//Create an app that has several functions that allows a user to perform different operations
+// on their name.
+
+//All of these functions should work even the name is uppercase, lowercase, or a mix of upper & lowercase
+
+//Function #1: isDynamite(name)
+//Checks if their name starts with d or e. If so, returns true. If not, returns false.
+
+//Function #2: isCute(name)
+//Checks if their name is 3 letters long or 4 letters long. If so, returns true. If not, returns false.
+
+//Function #3: isAwesomesauce(name)
+//Checks if their name ends with n or c. If so, returns true. If not, returns false.
+
+//Function #4: isHunkyDory(name)
+//Checks if their name includes the word 'dan' anywhere in their name. If so, returns trye. If not, returns false.
+
+//Function #5: nameCombiner(name1, name2)
+//Returns a combination of name1 and name2 by taking the first half of name1 and the second half of name2 and combining them together
+
+//Function #6: noVowels(name)
+//Returns a copy of the name without any vowels
+
+//Function #7: leetSpeak(name)
+//Returns a copy of the name where:
+  //All the e's are replaced with 3's
+  //All the o's are replaced with 0's
+  //All the t's are replaced with 7's
+  //All the l's are replaced with 1's
---
+++ playground.js
@@ -0,0 +1,9 @@
+//Playground.js
+
+let str1a = "abcdefghijklmnopqrstuvwxyz";
+let str1b = "Schneider";
+let str2 = "I lIkE tO tYpE iN AnNoYiNg WaYs!!!"
+let str3 = "Schneider is the worst teacher in the world";
+let str4 = "1) Get up 2) Eat Breakfast 3) Go to School";
+let str5b = "shenanigans"
+let str5a = "aeiou"
